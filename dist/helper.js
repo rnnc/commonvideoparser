efa51646bf6aca67c548e674edb7446eeca1a57b
rnnc <rohan_cy@outlook.com>
2018-05-01 16:38:02 -0400

    upgraded babel config

--- dist/helper.js
+++ dist/helper.js
@@ -1,1 +1,1 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0}),exports.stringCheck=stringCheck,exports.errorBuild=errorBuild;function stringCheck(a){return!("string"!=typeof a)||!!(a instanceof String)}function errorBuild(a,b,c){return b?`\n ** (${a}) -> ${b}\n ** ${c}`:`\n ** (${a}) -> ${c}`}
+"use strict";exports.__esModule=!0,exports.stringCheck=stringCheck,exports.errorBuild=errorBuild;function stringCheck(a){return!("string"!=typeof a)||!!(a instanceof String)}function errorBuild(a,b,c){return b?`\n ** (${a}) -> ${b}\n ** ${c}`:`\n ** (${a}) -> ${c}`}
